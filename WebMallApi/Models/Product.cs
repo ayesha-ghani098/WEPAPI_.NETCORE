45b934be8ac01359ed75a29a1f7550636cbdec32
ayesha-ghani098 <ayeshaghanichi@gmail.com>
2022-06-18 17:22:37 +0500

    product model added

---
+++ WebMallApi/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace WebMallApi.Models
+{
+    public class Product
+    {
+      public int Id { get; set; }  
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public int Size { get; set; }
+        public double Weight{ get; set; }
+    }
+}
